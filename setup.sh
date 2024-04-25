9ce364a53c57389c7a8235c87c69fb7ad16154f2
Alec Buchanan <abuchanan@and.digital>
2024-04-25 15:03:45 +0100

    updates to setups

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get install git-all
-git clone https://github.com/alecb7/mlx-week8.git
 wget -P ~/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ~/Miniconda3-latest-Linux-x86_64.sh
 ~/Miniconda3-latest-Linux-x86_64.sh -b
