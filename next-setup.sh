85e3d954c36ed932987cb8a4543603bb44432a11
Alec Buchanan <abuchanan@and.digital>
2024-04-25 15:01:52 +0100

    update to setups

--- next-setup.sh
+++ next-setup.sh
@@ -1,5 +1,4 @@
 conda activate base
-conda install cudatoolkit=11.0 -y
 pip install transformers datasets peft accelerate 
-conda install -c pytorch faiss-cpu=1.8.0
+conda install -c pytorch -c nvidia faiss-gpu=1.8.0
 conda install numpy scipy scikit-learn pytorch torchvision -c conda-forge
